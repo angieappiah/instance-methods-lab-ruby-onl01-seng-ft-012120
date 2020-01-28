1103208dfe4227ec03cb08e8083ea186aa753353
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 19:55:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+
+class dog 
+  def (dog.bark)
+    puts "woof!"
+end  
+
+class sitting_dog
+def name (dog)
+  @name = name
+  puts " The #{name}" is sitting".
+end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class talk_in_person
+  def 'greeting'
+    @greeting = "Hello World"
+    puts "{greeting}"
+  end
+end
+
+
+def'walking'
+person.do
+  phrase = "walking"
+  puts "The person is "{phrase}".
+end
