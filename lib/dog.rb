732f0f6c62bc32e04f3f67209a9e70a19972342f
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 19:44:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,14 @@
 
 class dog 
-  def dog.bark
+  def (dog.bark)
     puts "woof!"
 end  
+
+class sitting_dog
+def name (dog)
+  @name = name
+  puts " The #{name}" is sitting".
+end
+end
 
 dog = "sit"
